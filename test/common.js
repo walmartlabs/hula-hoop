a435995af5d4c60bf6bfb3af642135edc6ff635d
kpdecker <kpdecker@gmail.com>
2014-05-18 16:18:58 -0500

    Always sandbox sinon

--- test/common.js
+++ test/common.js
@@ -1,5 +1,32 @@
+
 global.chai = require('chai');
 global.expect = chai.expect;
 
 var sinonChai = require('sinon-chai');
 chai.use(sinonChai);
+
+var Mocha = require('mocha'),
+    sinon = require('sinon');
+
+sinon.config = {
+  injectIntoThis: true,
+  injectInto: null,
+  properties: ['spy', 'stub', 'mock', 'sandbox'],
+  useFakeTimers: false,
+  useFakeServer: false
+};
+
+var loadFiles = Mocha.prototype.loadFiles;
+Mocha.prototype.loadFiles = function() {
+  this.suite.beforeEach(function() {
+    var config = sinon.getConfig(sinon.config);
+    config.injectInto = this;
+    this.sandbox = sinon.sandbox.create(config);
+  });
+  this.suite.afterEach(function() {
+    this.sandbox.verifyAndRestore();
+  });
+
+  return loadFiles.apply(this);
+};
+
